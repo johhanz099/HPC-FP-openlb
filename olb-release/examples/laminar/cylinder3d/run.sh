c7c46e070e3888dc5843ae69dfd52a1f0d9cb34c
spinillac11 <spinillac@unal.edu.co>
2025-07-23 02:53:27 -0500

    Simulación ala funcionando

---
+++ olb-release/examples/laminar/cylinder3d/run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+PROG=./cylinder3d
+ARGS="--use-hwthread-cpus" # Procesadores logicos cómo slots
+CORES=$(nproc) 
+
+echo "Detectados $CORES procesadores lógicos."
+
+#Crear carpeta de salida
+mkdir -p data
+
+# Cabecera de CSV
+echo "cores,rt_s" > data/timings.csv
+
+for p in $(seq 1 $CORES); do
+  LOG=run_p${p}.log
+  echo "Ejecutando con $p procesos..."
+  mpirun $ARGS -np $p $PROG > data/$LOG 2>&1
+  rt=$(grep "measured time (rt)" data/$LOG \
+       | awk '{ print $6 }' \
+       | tr -d 's')
+  echo "$p,$rt" >> data/timings.csv
+done
